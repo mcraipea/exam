222e14fbcdfc3495782a79cbb8d1061c87dc2dcc
Maxime Craipeau <max@MacBook-de-Maxime.local>
2020-02-14 14:04:36 +0100

    all

--- exam.c
+++ exam.c
@@ -8,43 +8,53 @@ void    ft_putstr(char *s)
         write(1, s++, 1);
 }
 
-int     ft_atoi_spe(char *buffer, int *i)
+int check_digit(char c)
 {
-    int         result = 0;
-    int         virg = 0;
-    int         neg = 0;
-	int			verif = 0;
+    if ((c>='0') && (c<='9')) 
+		return 1;
+    return 0;
+}
 
-    while (buffer[*i] && buffer[*i] == ' ')
+int	check_char(char c)
+{
+	if (c < 48 && c > 57)
+		return (1);
+	return (0);
+}
+
+int     ft_atoi_spe(char *str, int *i)
+{
+    int         res = 0;
+    int         neg = 1;
+
+    while (str[*i] && str[*i] == ' ')
         *i += 1;
-    if (buffer[*i] == '-')
-    {
-        neg += 1;
-        *i += 1;
-    }
-    while (buffer[*i] && buffer[*i] >= '0' && buffer[*i] <= '9')
-    {
-        result = 10 * result + buffer[*i] - '0';
-        *i += 1;
-		verif = 1;
-    }
-    if (buffer[*i] == '.')
-    {
-        *i += 1;
-        virg += 1;
-    }
-     while (buffer[*i] && buffer[*i] >= '0' && buffer[*i] <= '9')
-        *i += 1;
-    while (buffer[*i] && buffer[*i] == ' ')
-        *i += 1;
-    if (neg == 1)
-        result *= -1;
-    if (virg > 0)
-        result += 1;
-	if (verif == 1)
-    	return (result);
-	else
+	if (check_char(str[*i]))
 		return (-10000);
+    if (str[*i] == '-')
+	{
+		*i += 1;
+		neg = -1;
+	}
+	if (check_char(str[*i]))
+		return (-10000);
+	for (res = 0; str[*i] && check_digit(str[*i]); *i += 1)
+        res = res * 10 + str[*i] - '0';
+	if (check_char(str[*i]))
+		return (-10000);
+    if (str[*i] == '.')
+        *i += 1;
+	if (check_char(str[*i]))
+		return (-10000);
+   //for (decplace = 1.; str[*i] && isdigit(str[*i]); *i++, decplace *= 10.)
+   //     res = res * 10. + (*str - '0');
+	while (str[*i] && check_digit(str[*i]))
+    	*i += 1;
+	if (check_char(str[*i]))
+		return (-10000);
+    while (str[*i] && str[*i] == ' ')
+        *i += 1;
+    return (neg * res);
 }
 
 int     main(int argc, char **argv)
@@ -61,7 +71,9 @@ int     main(int argc, char **argv)
 
     char        contour;
     int         x_pos;
+	int			x_pos_b;
     int         y_pos;
+	int			y_pos_b;
     int         width;
     int         width_b;
     int         height;
@@ -86,13 +98,17 @@ int     main(int argc, char **argv)
         }
         buffer[ret] = '\0';
         //parser la premiere ligne
-        if ((w = ft_atoi_spe(buffer, &i)) == 0)
+        if ((w = ft_atoi_spe(buffer, &i)) <= 0 || w > 300 || w == -10000)
 			return (1);
-        if ((h = ft_atoi_spe(buffer, &i)) == 0)
+        if ((h = ft_atoi_spe(buffer, &i)) == 0 || h > 300 || h == -10000)
 			return (1);
        	if ((c = buffer[i++]) < 33 || c > 126)
 			return (1);
-        if (buffer[i] == '\n')
+        while (buffer[i] && buffer[i] == ' ')
+			i++;
+		if (buffer[i] && buffer[i] != '\n')
+			return (1);
+		if (buffer[i] == '\n')
 			i++;
         //malloc et creation du tableau
         tab = malloc(h * sizeof(char*));
@@ -106,19 +122,32 @@ int     main(int argc, char **argv)
         while (buffer[i])
         {
             //parser les infos lignes a lignes
-            contour = buffer[i++];
-            if ((x_pos = ft_atoi_spe(buffer, &i)) == -10000)
+            while (buffer[i] && buffer[i] == ' ')
+				i++;
+			if ((contour = buffer[i++]) != 'r' && contour != 'R')
 				return (1);
-            if ((y_pos = ft_atoi_spe(buffer, &i)) == -10000)
+            if ((x_pos = ft_atoi_spe(buffer, &i)) < -2148000 || x_pos > 2147000 || x_pos == -10000)
 				return (1);
-            if ((width = ft_atoi_spe(buffer, &i) + x_pos) == -10000)
+            if ((y_pos = ft_atoi_spe(buffer, &i)) < -2148000 || y_pos > 2147000 || y_pos == -10000)
 				return (1);
-            if ((height = ft_atoi_spe(buffer, &i) + y_pos) == -10000)
+            if ((width = ft_atoi_spe(buffer, &i) + x_pos) < -2148000 || width > 2147000 || width == -10000)
+				return (1);
+            if ((height = ft_atoi_spe(buffer, &i) + y_pos) < -2148000 || height > 2147000 || height == -10000)
 				return (1);
             if ((c = buffer[i++]) < 33 || c > 126)
 				return (1);
+		//	printf("x = %i, y = %i, width = %i, height = %i, c = %c\n", x_pos, y_pos, width, height, c);
+        	while (buffer[i] && buffer[i] == ' ')
+				i++;
+		//	printf("buffer = %c\n", buffer[i]);
+			if (buffer[i] != '\0' && buffer[i] != '\n')
+				return (1);
+			if (buffer[i] == '\n')
+				i++;
+			x_pos_b = x_pos;
             if (x_pos < 0)
                 x_pos = 0;
+			y_pos_b = y_pos;
             if (y_pos < 0)
                 y_pos = 0;
             width_b = width;
@@ -127,9 +156,8 @@ int     main(int argc, char **argv)
             height_b = height;
             if (height > h)
                 height = h;
-            if (buffer[i] == '\n')
-                i++;
             //dessiner si grand R ou petit r
+		//	printf("test\n");
             if (contour == 'R')
             {
                 for (int j = y_pos; j < height; j++)
@@ -143,7 +171,7 @@ int     main(int argc, char **argv)
                 for (int j = y_pos; j < height; j++)
 	            {
                     for (int k = x_pos; k < width; k++)
-                        if (j == y_pos || j == height_b - 1 || k == x_pos || k == width_b - 1)
+                        if (j == y_pos_b || j == height_b - 1 || k == x_pos_b || k == width_b - 1)
                             tab[j][k] = c;
                 }
             }
